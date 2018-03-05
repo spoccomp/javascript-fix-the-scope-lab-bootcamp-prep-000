79f97bb8ba3da8214aae79439f1dc2366b1b627a
Richard Trapanese <richardtrapanese@gmail.com>
2018-03-05 20:14:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  
+
 }
 
 function add2(n) {
